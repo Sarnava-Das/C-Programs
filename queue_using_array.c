600057cdb15490fe97a4ecd97fafa33d34ab1447
DawnHavoc <sarnavadas2@gmail.com>
2023-11-18 19:28:41 +0530

    commit

--- queue_using_array.c
+++ queue_using_array.c
@@ -1,140 +1,150 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_SIZE 100
 
-int capacity; 
+
 struct Queue {
-    int front, rear, size;
-    int* array;
+    int *array;
+    int front, rear, maxSize;
 };
 
-struct Queue* createQueue() {
-    struct Queue* queue = (struct Queue*)malloc(sizeof(struct Queue));
-    if (!queue) {
-        printf("Memory allocation error.\n");
+
+void initializeQueue(struct Queue *queue, int maxSize) {
+    queue->array = (int *)malloc(maxSize * sizeof(int));
+    if (queue->array == NULL) {
+        printf("Memory allocation failed.\n");
         exit(1);
     }
-    queue->front = queue->rear = -1;
-    queue->size = 0;
-    queue->array = (int*)malloc(capacity * sizeof(int));
-    if (!queue->array) {
-        printf("Memory allocation error.\n");
-        exit(1);
-    }
-    return queue;
-}
 
-int isEmpty(struct Queue* queue) {
-    return (queue->size == 0);
+    queue->front = -1;
+    queue->rear = -1;
+    queue->maxSize = maxSize;
 }
 
 
-int isFull(struct Queue* queue) {
-    return (queue->size == capacity);
+int isEmpty(struct Queue *queue) {
+    return (queue->front == -1 && queue->rear == -1);
 }
 
 
-void enqueue(struct Queue* queue, int item) {
+int isFull(struct Queue *queue) {
+    return (queue->rear == queue->maxSize - 1);
+}
+
+
+void enqueue(struct Queue *queue, int value) {
     if (isFull(queue)) {
         printf("Queue is full. Cannot enqueue.\n");
         return;
     }
-    if (queue->front == -1) {
+
+    if (isEmpty(queue)) 
         queue->front = 0;
-    }
-    queue->rear = (queue->rear + 1) % capacity;
-    queue->array[queue->rear] = item;
-    queue->size++;
-    printf("%d has been enqueued into the queue.\n", item);
+    
+
+   
+    queue->array[++queue->rear] = value;
+    printf("Enqueued: %d\n", value);
 }
 
 
-int dequeue(struct Queue* queue) {
+void dequeue(struct Queue *queue) {
     if (isEmpty(queue)) {
         printf("Queue is empty. Cannot dequeue.\n");
-        return -1; 
+        return;
     }
-    int item = queue->array[queue->front];
-    queue->front = (queue->front + 1) % capacity;
-    queue->size--;
-    return item;
+
+    printf("Dequeued: %d\n", queue->array[queue->front]);
+
+    if (queue->front == queue->rear) {
+      
+        queue->front = -1;
+        queue->rear = -1;
+    } 
+    else 
+        queue->front++;
+    
 }
 
 
-int peep(struct Queue* queue) {
+void peep(struct Queue *queue) {
     if (isEmpty(queue)) {
         printf("Queue is empty. Cannot peep.\n");
-        return -1; 
+        return;
     }
-    return queue->array[queue->front];
+
+    printf("Peep: %d\n", queue->array[queue->front]);
 }
 
 
-void display(struct Queue* queue) {
+void displayQueue(struct Queue *queue) {
     if (isEmpty(queue)) {
         printf("Queue is empty.\n");
         return;
     }
+
     printf("Queue elements: ");
-    int i = queue->front;
-    for (int count = 0; count < queue->size; count++) {
+    for (int i = queue->front; i <= queue->rear; i++) {
         printf("%d ", queue->array[i]);
-        i = (i + 1) % capacity;
     }
+
     printf("\n");
 }
 
 int main() {
-    printf("Enter the size of the queue: ");
-    scanf("%d", &capacity);
+    struct Queue queue;
+    int maxSize, choice, value;
 
-    if (capacity <= 0 || capacity > MAX_SIZE) {
-        printf("Invalid capacity. Please enter a positive integer less than or equal to %d.\n", MAX_SIZE);
-        return 1;
-    }
+   
+    printf("Enter the maximum size of the queue: ");
+    scanf("%d", &maxSize);
 
-    struct Queue* queue = createQueue();
-    int choice, item;
+    initializeQueue(&queue, maxSize);
 
-    while (1) {
-        printf("\nQueue Menu:\n");
-        printf("1. Enqueue (Insert)\n");
-        printf("2. Dequeue (Delete)\n");
-        printf("3. Peep (Front element)\n");
-        printf("4. Display\n");
-        printf("5. Exit\n");
+    do {
+    
+        printf("\n---- Menu ----\n");
+        printf("1. Enqueue\n");
+        printf("2. Dequeue\n");
+        printf("3. Peep\n");
+        printf("4. Display Queue\n");
+        printf("0. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
         switch (choice) {
             case 1:
-                printf("Enter element to enqueue: ");
-                scanf("%d", &item);
-                enqueue(queue, item);
+               
+                printf("Enter the value to enqueue: ");
+                scanf("%d", &value);
+                enqueue(&queue, value);
                 break;
+
             case 2:
-                item = dequeue(queue);
-                if (item != -1) {
-                    printf("Dequeued element: %d\n", item);
-                }
+             
+                dequeue(&queue);
                 break;
+
             case 3:
-                item = peep(queue);
-                if (item != -1) {
-                    printf("Front element: %d\n", item);
-                }
+              
+                peep(&queue);
                 break;
+
             case 4:
-                display(queue);
+              
+                displayQueue(&queue);
                 break;
-            case 5:
-                free(queue->array);
-                free(queue);
-                exit(0);
+
+            case 0:
+            
+                break;
+
             default:
                 printf("Invalid choice. Please try again.\n");
         }
-    }
+    } while (choice != 0);
+
+    
+    free(queue.array);
 
     return 0;
 }
